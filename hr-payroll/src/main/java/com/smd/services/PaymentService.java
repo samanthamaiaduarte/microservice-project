aeb59fa48811f11b80c9226d2df494ad31f73b39
samanthamaiaduarte <sam.maiaduarte@outlook.com>
2024-06-13 20:50:35 -0300

    hr-worker implementation - OpenFeign

--- hr-payroll/src/main/java/com/smd/services/PaymentService.java
+++ hr-payroll/src/main/java/com/smd/services/PaymentService.java
@@ -1,36 +1,25 @@
 package com.smd.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 
 import com.smd.entities.Payment;
 import com.smd.entities.Worker;
 import com.smd.exceptions.RecordNotFoundException;
+import com.smd.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient feignClient;
 
 	public Payment getPayment(Long workerId, Integer days) {
-		Map<String, String> params = new HashMap<>();
-		params.put("id", workerId.toString());
-		
 		try {
-			Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, params);
+			Worker worker = feignClient.findById(workerId).getBody();
 			return new Payment(worker.getName(), worker.getDailyIncome(), days);
 		}
-		catch(RestClientException e) {
+		catch(RuntimeException e) {
 			throw new RecordNotFoundException("Worker not found.");
 		}
 	}
