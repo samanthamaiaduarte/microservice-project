d8420882bdc275e1fa56a32891e7c678ecff254f
samanthamaiaduarte <sam.maiaduarte@outlook.com>
2024-06-13 23:00:23 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/smd/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/smd/resources/PaymentResource.java
@@ -1,13 +1,16 @@
 package com.smd.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.smd.entities.Payment;
+import com.smd.exceptions.RecordNotFoundException;
 import com.smd.services.PaymentService;
 
 @RestController
@@ -17,9 +20,15 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService service;
 	
+	@HystrixCommand(ignoreExceptions = { RecordNotFoundException.class }, fallbackMethod = "getPaymentAlt")
 	@GetMapping("/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 		Payment payment = service.getPayment(workerId, days);
 		return ResponseEntity.ok(payment);
+	}
+	
+	public ResponseEntity<Payment> getPaymentAlt(Long workerId, Integer days) {
+		Payment payment = new Payment("Worker is out", 0.0, days);
+		return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(payment);
 	}
 }
